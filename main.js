d2389f5723307dc8ba48dcf5cce8329a709948db
tringuyen <tri.nh@hotmail.com>
2021-11-28 14:10:16 +0700

    update

--- main.js
+++ main.js
@@ -420,6 +420,19 @@ app.post('/quanlymay/', function (req, res) {
     });
 });
 
+app.post('/danhsachdonhang/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('INSERT INTO danhsachdonhang VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.get('/getloaimay/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
@@ -506,6 +519,19 @@ app.get('/getquanlymay/', function (req, res) {
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
     pool.query('select * from quanlymay', function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+app.get('/getdanhsachdonhang/', function (req, res) {
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+    pool.query('select * from danhsachdonhang', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
