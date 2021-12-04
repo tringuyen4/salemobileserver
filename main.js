bb2a7310ffa3afc6886a85036caa224b78ae0a1e
tringuyen <tri.nh@hotmail.com>
2021-12-04 14:22:07 +0700

    'update'

--- main.js
+++ main.js
@@ -448,6 +448,19 @@ app.post('/danhsachdonhang/', function (req, res) {
     });
 });
 
+app.post('/danhsachdonhangtemp/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('INSERT INTO danhsachdonhangtemp VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,DEFAULT)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.get('/getloaimay/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
