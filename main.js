4214214cc4c2c96b88e70ee25e0ef32e3e12f616
tringuyen <tri.nh@hotmail.com>
2021-12-04 15:51:20 +0700

    'update'

--- main.js
+++ main.js
@@ -578,6 +578,19 @@ app.post('/getdanhsachdonhangtheonguoimua/', function (req, res) {
     });
 });
 
+app.post('/getdanhsachdonhangtheonguoimuareal/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('select * from danhsachdonhang where sodienthoaikhachhang = ($1)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 app.put('/updatetrangthaidonhang/', function (req, res) {
      var postData = req.body;
      res.header("Access-Control-Allow-Origin", "*");
