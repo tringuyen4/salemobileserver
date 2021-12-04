2659101157d99116444eb87b3a79eed25bd83e19
tringuyen <tri.nh@hotmail.com>
2021-12-04 14:33:48 +0700

    'update'

--- main.js
+++ main.js
@@ -598,7 +598,7 @@ app.put('/updatetrangthaidonhang/', function (req, res) {
      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
      res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-     pool.query('DELETE FROM danhsachdonhang where madonhang = ($1)', postData, function (error, results, fields) {
+     pool.query('DELETE FROM danhsachdonhangtemp where madonhang = ($1)', postData, function (error, results, fields) {
          if (error) throw error;
          res.end(JSON.stringify(results.rows));
      });
