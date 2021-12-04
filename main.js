aab162beb08e28b735ffe115ffcf9ca5a6917272
tringuyen <tri.nh@hotmail.com>
2021-12-04 17:42:12 +0700

    'update'

--- main.js
+++ main.js
@@ -70,6 +70,18 @@ app.post('/authen/', function (req, res) {
         res.end(JSON.stringify(results.rows));
     });
 });
+app.post('/getdanhsachdonhangtheomadonhang/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('select * from danhsachdonhang where madonhang = ($1)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
 
 //input user
 app.post('/user/', function (req, res) {
