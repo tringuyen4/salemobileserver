147f9a8d7a65e72f694521797d6095b40964d8ca
tringuyen <tri.nh@hotmail.com>
2021-12-10 16:06:38 +0700

    update

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ app.post('/gettonggiatienreal/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('select SUM (CAST (gia AS INTEGER)) AS total from danhsachdonhang where madonhang = ($1)', postData, function (error, results, fields) {
+    pool.query('select SUM (CAST (gia AS INTEGER)) AS total, Count(*) as totalitem from danhsachdonhang where madonhang = ($1)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
@@ -103,7 +103,7 @@ app.post('/gettonggiatientemp/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('select SUM (CAST (gia AS INTEGER)) AS total from danhsachdonhangtemp where madonhang = ($1)', postData, function (error, results, fields) {
+    pool.query('select SUM (CAST (gia AS INTEGER)) AS total, Count(*) as totalitem from danhsachdonhangtemp where madonhang = ($1)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
